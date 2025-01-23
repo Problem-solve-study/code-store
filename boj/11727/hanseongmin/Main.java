5581c5aa81cbc1b3c057ef857a0a7832cbfcec81
한성민 <b12e34e56@naver.com>
2025-01-23 19:49:58 +0900

    Solve: BOJ 11727

---
+++ boj/11727/hanseongmin/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    static int[] dp;
+    static final int MOD = 10007;
+
+    public static void main(String[] args) throws Exception {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        var bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[1001];
+        dp[1] = 1; dp[2] = 3;
+
+        bw.write(String.valueOf(getAns(n)));
+        bw.flush();
+    }
+
+    public static int getAns(int n) {
+        if (dp[n] != 0) return dp[n];
+
+        dp[n] = (getAns(n - 1) + 2 * getAns(n - 2)) % MOD;
+        return dp[n];
+    }
+}
