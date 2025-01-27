23a042908cdfb6ecd7e9deaedaccb551bf2d8919
hmk5940 <102127320+hmk5940@users.noreply.github.com>
2025-01-27 09:02:25 +0900

    Solve: BOJ 12107

---
+++ boj/12107/hongmingi/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int input = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		if(input!=1) {
+			System.out.println("A");
+		}
+		else {
+			System.out.println("B");
+		}
+		
+	}
+}
