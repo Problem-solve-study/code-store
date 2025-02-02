a9dc5bfe380e9f735f14c5e630155e392c810bae
백대환 <nm09090@naver.com>
2025-02-02 22:05:43 +0900

    Refactor: BOJ 1821

--- boj/1821/baekdaehwan/Main.java
+++ boj/1821/baekdaehwan/Main.java
@@ -1,11 +1,11 @@
-// 	33604KB	348ms
+// 15792KB	116ms
 
 import java.util.*;
 import java.io.*;
 
 public class Main {
     static int n, m;
-    static Stack<Integer> a;
+    static int[] a;
     static int v;
     static int s;
     static int[][] t;
@@ -14,7 +14,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        a = new Stack<>();
+        a = new int[n+1];
         t = new int[2][n];
         t[0][0] = 1;
         for (int i=0; i<n-1; i++) {
@@ -26,29 +26,27 @@ public class Main {
             s = (i+1)&1;
         }
         v = 0;
-        p(n);
+        p(0);
     }
 
-    static void p(int r) {
-        if (r == 0) solve();
+    static void p(int i) {
+        if (i == n) solve();
         else {
-            for (int i=1; i<=n; i++) {
-                if ((v & 1<<i) != 0) continue;
-                a.push(i);
-                v |= 1<<i;
-                p(r-1);
-                a.pop();
-                v ^= 1<<i;
+            for (int n = 1; n<= Main.n; n++) {
+                if ((v & 1<<n) != 0) continue;
+                v |= 1<<n;
+                a[i+1] = n;
+                p(i+1);
+                v ^= 1<<n;
             }
         }
     }
 
     static void solve() {
         int res = 0;
-        int i = 0;
-        for (int x: a) res += x * t[s][i++];
+        for (int i=0; i<n; i++) res += a[i+1] * t[s][i];
         if (res == m) {
-            for (int x: a) System.out.print(x+" ");
+            for (int i=0; i<n; i++) System.out.print(a[i+1]+" ");
             System.exit(0);
         }
     }
