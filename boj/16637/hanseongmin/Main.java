5747ed87654305fc0b382a12c4d1116132fa573d
한성민 <b12e34e56@naver.com>
2025-02-07 01:04:01 +0900

    Refactor: BOJ 16637

--- boj/16637/hanseongmin/Main.java
+++ boj/16637/hanseongmin/Main.java
@@ -1,6 +1,7 @@
-// 16536KB, 136ms
+// 15284KB, 124ms
 
 import java.io.*;
+import java.util.ArrayDeque;
 
 public class Main {
     static int res = Integer.MIN_VALUE;
@@ -24,13 +25,13 @@ public class Main {
         if (limit == cnt) {
             return;
         }
-        
+
         for (int i = 1; i < f.length(); i += 2) {
             //인접한 연산자는 선택하면 안됨
             if (bracket[i]) continue;
             if (i - 2 >= 0 && bracket[i - 2]) continue;
             if (i + 2 < f.length() && bracket[i + 2]) continue;
-            
+
             bracket[i] = true;
             select(limit, cnt + 1, bracket);
             bracket[i] = false;
@@ -39,77 +40,44 @@ public class Main {
 
     public static int getValue(boolean[] bracket) {
         //수식을 계산
-        
         //괄호가 씌여진 연산자를 먼저 계산하여 새로운 식을 생성
-        StringBuilder sb = new StringBuilder();
+        ArrayDeque<Integer> operand = new ArrayDeque<>();
+        ArrayDeque<Character> operator = new ArrayDeque<>();
         for (int i = 0; i < f.length(); i++) {
-            if (bracket[i]) {
-                for (int j = sb.length() - 1; j >= 0; j--) {
-                    if ((sb.charAt(j) == '-' && j - 1 >= 0 && !Character.isDigit(sb.charAt(j - 1))) || Character.isDigit(sb.charAt(j))) { 
-                        sb.deleteCharAt(j);
-                    } else {
-                        break;
-                    }
+            if (Character.isDigit(f.charAt(i))) {
+                operand.addLast(f.charAt(i) - '0');
+            } else {
+                if (bracket[i] && i != 1) {
+                    int o1 = operand.removeLast();
+                    int o2 = f.charAt(i + 1) - '0';
+                    calc(operand, o1, o2, f.charAt(i++), false);
+                } else {
+                    operator.add(f.charAt(i));
                 }
-                sb.append(calc(f.charAt(i - 1) - '0', f.charAt(i + 1) - '0', f.charAt(i)));
-                i++;
-            } else {
-                sb.append(f.charAt(i));
-            }
-        }
-        
-        String newF = sb.toString();
-        int i = 0;
-        sb = new StringBuilder();
-        for (; i < newF.length(); i++) {
-            if ((i == 0 && newF.charAt(i) == '-') || Character.isDigit(newF.charAt(i))) { 
-                sb.append(newF.charAt(i));
-            } else {
-                break;
-            }
-        }
-        int value = Integer.parseInt(sb.toString());
-        for (; i < newF.length(); i++) {
-            if (!Character.isDigit(newF.charAt(i))) {
-                int next = getNextP(i, newF);
-                value = calc(value, next, newF.charAt(i));
-                if (next < 0) i++;
             }
         }
 
-        return value;
+        while (operand.size() > 1) {
+            calc(operand, operand.removeFirst(), operand.removeFirst(), operator.removeFirst(), true);
+        }
+
+        return operand.peek();
     }
 
-    public static int getNextP(int idx, String f) {
-        //해당 연산자의 위치로부터 오른쪽 피연산자 추출
-        StringBuilder p = new StringBuilder();
-
-        for (int i = idx + 1; i < f.length(); i++) {
-            if ((i == idx + 1 && f.charAt(i) == '-') || Character.isDigit(f.charAt(i))) {
-                p.append(f.charAt(i));
-            } else {
-                break;
-            }
-        }
-
-        return Integer.parseInt(p.toString());
-    }
-
-    public static int calc(int p1, int p2, char y) {
-        //연산자 1개, 피연산자 2개로 값을 계산
-        int value = 0;
-
-        switch (y) {
+    public static void calc(ArrayDeque<Integer> operand, int a, int b, char o, boolean isFirst) {
+        switch (o) {
             case '+':
-                value = p1 + p2;
+                if (isFirst) operand.addFirst(a + b);
+                else operand.addLast(a + b);
                 break;
             case '-':
-                value = p1 - p2;
+                if (isFirst) operand.addFirst(a - b);
+                else operand.addLast(a - b);
                 break;
             case '*':
-                value = p1 * p2;
+                if (isFirst) operand.addFirst(a * b);
+                else operand.addLast(a * b);
+                break;
         }
-
-        return value;
     }
 }
