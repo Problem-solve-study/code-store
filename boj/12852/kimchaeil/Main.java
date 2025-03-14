f8f54d8099c0c76219b9be4461f1530e76559fcd
김채일 <rlacodlf11@naver.com>
2025-03-14 15:11:33 +0900

    Refactor: BOJ 12852 연산법 변경 및 불필요한 배열 제거

--- boj/12852/kimchaeil/Main.java
+++ boj/12852/kimchaeil/Main.java
@@ -1,6 +1,6 @@
 //문제: BOJ 12852번 1로 만들기 2
-//메모리: 20444 KB
-//시간: 148 ms
+//메모리: 20868 KB
+//시간: 132 ms
 
 /*
  * 문제에서 주어진 3가지 연산이 모두 값이 줄어드는 연산이므로 DP를 선택
@@ -9,7 +9,8 @@
  * 그래서 divide배열({3,2,1})과 minus({0,0,1})배열을 이용해 for문으로 수정하였다.
  * 또한 모듈러 연산을 통해 나누어 떨어지는지에 대한 연산에 대한 오버헤드와 비효율적인 경로 추적때문에
  * 연산을 반대로 하여 1에서 n으로 가는 DP로 변경하였다.
- * divide배열과 minus배열의 이름을 가독성을 위해 time와 plus로 각각 변경하였다.
+ * divide배열과 minus배열의 이름을 가독성을 위해 times와 plus로 각각 변경하였다.
+ * +) times배열과 plus배열 삭제 및 연산 방법 변경
  */
 
 import java.io.*;
@@ -23,13 +24,10 @@ public class Main {
 		for (int i = 2; i <= n; i++)
 			dp[0][i] = Integer.MAX_VALUE;
 
-		int[] times = { 3, 2, 1 };
-		int[] plus = { 0, 0, 1 };
 		int idx = 1;
-
 		for (int i = 1; i < n; i++) {
-			for (int j = 0; j < 3; j++) {
-				idx = i * times[j] + plus[j]; // 다음 값 연산
+			for (int j = 3; j > 0; j--) {
+				idx = i * j + (1-(j>>1)); // 다음 값 연산
 				if (idx > n) // 다음 값이 n보다 크면 X
 					continue;
 				if (dp[0][idx] > dp[0][i] + 1) { // 다음 값에 더 빨리 갈수 있는 경로라면
