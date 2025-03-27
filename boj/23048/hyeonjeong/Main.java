3926db7aced1337d631240dc0c13c76e24234fc2
HyeonJeong <117988360+wgwjh05169@users.noreply.github.com>
2025-03-27 09:50:03 +0900

    Refactor: BOJ 23048 출력 시간 단축

--- boj/23048/hyeonjeong/Main.java
+++ boj/23048/hyeonjeong/Main.java
@@ -1,4 +1,5 @@
 // 327860KB 2064ms
+// -> 	28236KB 212ms
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -8,6 +9,7 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 // 에라토스테네스의 체처럼 소수부터 그의 배수를 같은 수로 업데이트
+// sysout이랑 bw랑,,, String.format이랑 StringBuilder가,, 차이가 많이 나네요
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -37,10 +39,18 @@ public class Main {
             }
         }
 
-        bw.write(String.format("%d\n", count));
-        for (int i = 1; i <= n; i ++) {
-            bw.write(String.format("%d ", colors[i]));
-        }
-        bw.flush();
+        // bw.write(String.format("%d\n", count));
+        // for (int i = 1; i <= n; i ++) {
+        //     bw.write(String.format("%d ", colors[i]));
+        // }
+        // bw.flush();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(count).append('\n');
+    	for (int i = 1; i <= n; i++) {
+    		sb.append(colors[i]).append(' ');
+    	}
+        
+    	System.out.println(sb);
     }
 }
