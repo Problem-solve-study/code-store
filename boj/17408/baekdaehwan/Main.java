d15850522b2ceb85115e6b253a74810a645cd047
DoDoGaMaRu <nm09090@naver.com>
2025-04-08 10:27:14 +0900

    Refactor: BOJ 17408 최댓값 2개 선별 로직 수정

--- boj/17408/baekdaehwan/Main.java
+++ boj/17408/baekdaehwan/Main.java
@@ -1,5 +1,5 @@
 /**
- * 	65836KB	420ms
+ * 	65904KB	408ms
  * 
  * [사고흐름]
  * 원소 두 개짜리 세그트리라고 생각했습니다.
@@ -59,32 +59,10 @@ public class Main {
     }
     
     static void max2(int[] dest, int[] a, int[] b) {
-    	dest[0] = Integer.MIN_VALUE;
-    	dest[1] = Integer.MIN_VALUE;
-		if (a[0]>=dest[0]) {
-			dest[1] = dest[0];
-			dest[0] = a[0];
-		} else if (a[0]>=dest[1]) {
-			dest[1] = a[0];
-		}
-		if (a[1]>=dest[0]) {
-			dest[1] = dest[0];
-			dest[0] = a[1];
-		} else if (a[1]>=dest[1]) {
-			dest[1] = a[1];
-		}
-		if (b[0]>=dest[0]) {
-			dest[1] = dest[0];
-			dest[0] = b[0];
-		} else if (b[0]>=dest[1]) {
-			dest[1] = b[0];
-		}
-		if (b[1]>=dest[0]) {
-			dest[1] = dest[0];
-			dest[0] = b[1];
-		} else if (b[1]>=dest[1]) {
-			dest[1] = b[1];
-		}
+    	int ai = 0;
+    	int bi = 0;
+    	dest[0] = a[ai]>b[bi]? a[ai++]:b[bi++];
+    	dest[1] = Math.max(a[ai], b[bi]);
     }
     
     static int ni() throws Exception {
