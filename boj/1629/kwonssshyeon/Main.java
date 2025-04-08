b50323e83bee3948b57b2714cca82f06f67604db
kwonssshyeon <104684033+kwonssshyeon@users.noreply.github.com>
2025-04-08 10:53:24 +0900

    Solve: 0408 E BOJ 1629

---
+++ boj/1629/kwonssshyeon/Main.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 이젠 익숙하죠
+ */
+public class Main {
+    static long a, n, c;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        a = Long.parseLong(st.nextToken());
+        n = Long.parseLong(st.nextToken());
+        c = Long.parseLong(st.nextToken());
+        System.out.print(mult());
+    }
+
+    static long mult() {
+        long result = 1;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                result = (result * a) % c;
+            }
+            a = (a * a) % c;
+            n /= 2;
+        }
+        return result;
+    }
+}
