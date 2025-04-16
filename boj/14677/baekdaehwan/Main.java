d41b060cc3567e8b9ea91a0114aafd82b75de6a4
백대환 <nm09090@naver.com>
2025-04-16 10:16:23 +0900

    Comment: BOJ 14677

--- boj/14677/baekdaehwan/Main.java
+++ boj/14677/baekdaehwan/Main.java
@@ -37,3 +37,38 @@ public class Main {
 		return max;
 	}
 }
+
+// DP 코드
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+// 	static int N;
+// 	static int[] A; 
+// 	static int[][][] DP;
+
+// 	static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+// 	public static void main(String[] args) throws Exception {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		N = Integer.parseInt(br.readLine())*3;
+// 		A = new int[N];
+// 		char[] c = br.readLine().toCharArray();
+// 		for (int i=0; i<N; ++i) A[i] = c[i]=='B'? 0:(c[i]=='L'? 1:2);
+// 		DP = new int[N][N][3];
+// 		for (int i=0; i<N; ++i) {
+// 			for (int j=0; j<N; ++j) Arrays.fill(DP[i][j], -1);
+// 		}
+		
+// 		System.out.println(bt(0, N-1, 0));
+//     }
+	
+// 	static int bt(int l, int r, int c) {
+// 		if (l>r) return 0;
+// 		if (DP[l][r][c] == -1) {
+// 			DP[l][r][c] = 0;
+// 			if (A[l] == c) DP[l][r][c] = Math.max(DP[l][r][c], bt(l+1, r, (c+1)%3)+1);
+// 			if (A[r] == c) DP[l][r][c] = Math.max(DP[l][r][c], bt(l, r-1, (c+1)%3)+1);
+// 		}
+// 		return DP[l][r][c];
+// 	}
+// }
