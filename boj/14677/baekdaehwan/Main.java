8d82a3fdc10cf9e08cf481895ee777cd31cfee0e
백대환 <nm09090@naver.com>
2025-04-16 10:15:17 +0900

    Refactor: BOJ 14677

--- boj/14677/baekdaehwan/Main.java
+++ boj/14677/baekdaehwan/Main.java
@@ -1,5 +1,6 @@
 /**
- * 91136KB	188ms
+ * 13880KB	68ms
+ * 실행시간, 숏코딩 둘 다 1등 가져갑니다
  * 
  * [사고흐름]
  * 뭐야, 단순 DP네?
@@ -12,35 +13,27 @@
 
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
 	static int N;
-	static int[] A; 
-	static int[][][] DP;
-
+	static int[] A;
+	static boolean[][] visited;
 	static StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine())*3;
 		A = new int[N];
+		visited = new boolean[N][N];
 		char[] c = br.readLine().toCharArray();
 		for (int i=0; i<N; ++i) A[i] = c[i]=='B'? 0:(c[i]=='L'? 1:2);
-		DP = new int[N][N][3];
-		for (int i=0; i<N; ++i) {
-			for (int j=0; j<N; ++j) Arrays.fill(DP[i][j], -1);
-		}
-		
 		System.out.println(bt(0, N-1, 0));
     }
-	
 	static int bt(int l, int r, int c) {
-		if (l>r) return 0;
-		if (DP[l][r][c] == -1) {
-			DP[l][r][c] = 0;
-			if (A[l] == c) DP[l][r][c] = Math.max(DP[l][r][c], bt(l+1, r, (c+1)%3)+1);
-			if (A[r] == c) DP[l][r][c] = Math.max(DP[l][r][c], bt(l, r-1, (c+1)%3)+1);
-		}
-		return DP[l][r][c];
+		if (l>r || visited[l][r]) return 0;
+		visited[l][r] = true;
+		int max = 0;
+		if (A[l] == c) max = Math.max(max, bt(l+1, r, (c+1)%3)+1);
+		if (A[r] == c) max = Math.max(max, bt(l, r-1, (c+1)%3)+1);
+		return max;
 	}
 }
