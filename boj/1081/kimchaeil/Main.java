825a7af942aebc0141a68e4d4bf8fe42c230e48a
LES8638 <105543590+LES8638@users.noreply.github.com>
2025-04-21 14:42:55 +0900

    Comment: 주석 추가

--- boj/1081/kimchaeil/Main.java
+++ boj/1081/kimchaeil/Main.java
@@ -11,31 +11,34 @@ import java.io.*;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		int l = nextInt(), r = nextInt();
-		int frontl = l, nowl = 0, backl = 0;
-		int frontr = r, nowr = 0, backr = 0;
+		int frontl = nextInt(), nowl = 0, backl = 0;
+		// frontl: L의 현재 보고있는 자리 앞부분, nowl: L의 현재 보고있는 자리수, backl: L의 현재 보고있는 자리 뒷부분
+		int frontr = nextInt(), nowr = 0, backr = 0;
+		// frontr: R의 현재 보고있는 자리 앞부분, nowr: R의 현재 보고있는 자리수, backr: R의 현재 보고있는 자리 뒷부분
 		long result = 0, temp = 1, digit = 0;
-		while (backr != r) {
-			backl = (int) (backl + nowl * digit);
-			backr = (int) (backr + nowr * digit);
+		// digit: 현재 보고 있는 자리
+		while (frontr + nowr != 0) {
+			// L과 R의 보고 있는 자리를 앞으로
+			backl = (int) (backl + digit * nowl);
+			backr = (int) (backr + digit * nowr);
 			nowl = frontl % 10;
 			nowr = frontr % 10;
 			frontl /= 10;
 			frontr /= 10;
-			temp *= 10;
-			digit = temp / 10;
-			if (nowl == nowr && frontl == frontr) {
-				result += nowl * (backr - backl + 1);
-			} else {
-				result += nowl * (digit - backl);
-				result += nowr * (backr + 1);
-				if (frontl == frontr) {
-					for (int i = nowl + 1; i < nowr; i++)
-						result += i * digit;
-				} else {
-					result += (45 - ((nowl * nowl + nowl) >> 1)) * digit;
-					result += ((nowr * nowr - nowr) >> 1) * digit;
-					result += 45 * digit * (frontr - frontl - 1);
+			temp = (digit = temp) * 10;
+
+			if (nowl == nowr && frontl == frontr) { // 앞부분과 현재 자리가 모두 같다면
+				result += (backr - backl + 1) * nowl; // 현재 자리가 나오는 횟수 = R 뒷부분 - L 뒷부분 + 1
+			} else { // 앞부분과 현재자리 중 하나라도 다르다면
+				result += (digit - backl) * nowl; // L의 현재 자리수가 front가 바뀌지 않는 동안은 (digit - L의 뒷부분)만큼 나옴
+				result += (backr + 1) * nowr; // R의 현재 자리수가 front가 바뀌지 않는 동안은 (R의 뒷부분 + 1)만큼 나옴
+				if (frontl == frontr) { // 만약 앞부분만 같다면
+					for (int i = nowl + 1; i < nowr; i++) // L의 현재 자리수보다 크고, R의 현재 자리수보다 작은 수들이 digit만큼 나옴
+						result += digit * i;
+				} else { // 앞부분이 다르다면(현재 자리는 같아도 됨)
+					result += digit * (45 - ((nowl * nowl + nowl) >> 1)); // (L의 현재 자리수 + 1) ~ 9가 digit만큼 나옴
+					result += digit * ((nowr * nowr - nowr) >> 1); // 1 ~ (R의 현재 자리수 - 1)가 digit만큼 나옴
+					result += digit * 45 * (frontr - frontl - 1); // 1 ~ 9가 (R의 앞부분 - L의 앞부분 - 1) * digit만큼 나옴
 				}
 			}
 		}
