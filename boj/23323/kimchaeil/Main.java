d5bcf0f42946a52140fb073a035a874284b375f9
LES8638 <105543590+LES8638@users.noreply.github.com>
2025-08-21 09:44:43 +0900

    Refactor: 23323 황소 다마고치 코드 개선

--- boj/23323/kimchaeil/Main.java
+++ boj/23323/kimchaeil/Main.java
@@ -10,20 +10,18 @@
  * -> 황소에게 먹이를 주지 않고 밤을 지내 체력이 1이 되게 한다.
  * -> 체력이 1이 된 날부터 먹이를 1씩 준다.
  * -> 먹이의 수만큼 생존 일이 늘어난다.
- * -> 먹이를 주지 않고 체력이 1이 될 때까지 최초 체력의 (비트수-1)일이다.
- * -> 정답은 (n의 비트수 - 1 + m)
+ * -> 먹이를 주지 않고 체력이 1이 될 때까지 최초 체력의 (비트수)일이다.
+ * -> 정답은 (n의 비트수 + m)
+ * -> n의 비트 수는 log_{2}(n)+1이고 이는 log(n*2)/log(2)로 표현된다.
+ * -> 즉, log(n*2)/log(2) + m
  */
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
         StringBuilder sb = new StringBuilder();
-        long n, result;
-        int T = (int) nextLong();
-        while (T-- > 0) {
-            for (result = 0, n = nextLong(); n > 0; n >>= 1, result++) ;
-            sb.append(result + nextLong()).append('\n');
-        }
+        for (int T = (int) nextLong(); T > 0; T--)
+            sb.append((long) (Math.log(nextLong() << 1) / Math.log(2)) + nextLong()).append('\n');
         System.out.println(sb);
     }
 
