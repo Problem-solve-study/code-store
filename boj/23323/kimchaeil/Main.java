f683f3be435644821bf455915a36a3f90dcc580f
LES8638 <105543590+LES8638@users.noreply.github.com>
2025-08-21 09:30:06 +0900

    Solve: 0821 E BOJ 23323

---
+++ boj/23323/kimchaeil/Main.java
@@ -0,0 +1,38 @@
+//문제: 23323번 황소 다마고치
+//메모리: 11720 KB
+//시간: 72 ms
+
+/*
+ * 매일 밤, 황소의 체력이 절반이 된다.
+ * -> 체력이 많을수록 체력의 손실이 크다.
+ * -> 최소한의 체력으로 밤을 지나게 한다.
+ * -> 다음날 체력이 0이 되지 않는 최소한의 체력은 2다.
+ * -> 황소에게 먹이를 주지 않고 밤을 지내 체력이 1이 되게 한다.
+ * -> 체력이 1이 된 날부터 먹이를 1씩 준다.
+ * -> 먹이의 수만큼 생존 일이 늘어난다.
+ * -> 먹이를 주지 않고 체력이 1이 될 때까지 최초 체력의 (비트수-1)일이다.
+ * -> 정답은 (n의 비트수 - 1 + m)
+ */
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        long n, result;
+        int T = (int) nextLong();
+        while (T-- > 0) {
+            for (result = 0, n = nextLong(); n > 0; n >>= 1, result++) ;
+            sb.append(result + nextLong()).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    static long nextLong() throws Exception {
+        long n, c;
+        while ((c = System.in.read()) < 48) ;
+        n = c & 15;
+        while ((c = System.in.read()) > 47)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+}
