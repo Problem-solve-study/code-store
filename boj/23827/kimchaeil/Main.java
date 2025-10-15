d544187f5c9d454eef2795ae8a841798ad15b668
LES8638 <105543590+LES8638@users.noreply.github.com>
2025-10-15 13:43:27 +0900

    Refactor: 23827

--- boj/23827/kimchaeil/Main.java
+++ boj/23827/kimchaeil/Main.java
@@ -1,6 +1,6 @@
 //문제: 23827번 수열 (Easy)
-//메모리: 16004 KB
-//시간: 144 ms
+//메모리: 12088 KB
+//시간: 124 ms
 
 /*
  * 1 <= i < j <=N 인 모든 정수쌍 (i,j)
@@ -10,21 +10,17 @@
  * A1*(A2+A3+A4+A5) + A2*(A3+A4+A5) + ... + A4*A5
  * 로 표현할 수 있다.
  * 이를 이용해 문제를 풀었다.
+ *
+ * refactor)
+ * A1*A2 + (A1+A2)*A3 + ...
+ * 처럼 반대로 묶는 방법으로 수정
  */
 
 public class Main {
     public static void main(String[] args) throws Exception {
         int n = nextInt();
-        int[] arr = new int[n + 1], sum = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            arr[i] = nextInt();
-            sum[i] = (sum[i - 1] + arr[i]) % 1_000_000_007;
-        }
-        long result = 0, temp = sum[n];
-        for (int i = 1; i < n; i++) {
-            result += ((temp - sum[i]) * arr[i]) % 1_000_000_007;
-            result %= 1_000_000_007;
-        }
+        long a = 0, b = nextInt(), result = 0;
+        while (n-- > 1) result = (result + (a += b) * (b = nextInt())) % 1_000_000_007;
         System.out.println(result);
     }
 
